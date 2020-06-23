d8c3f0aba7c5d64379823ae3514a8d3da519adab
Schnecke <eiweala@live.com>
2020-06-23 15:29:08 +0300

    first push

---
+++ js/main.js
@@ -0,0 +1,591 @@
+let repository = [
+  {
+    id: 572,
+    name: "Sandman",
+    slug: "572-sandman",
+    powerstats: {
+      intelligence: 44,
+      strength: 75,
+      speed: 46,
+      durability: 90,
+      power: 100,
+      combat: 60,
+    },
+    appearance: {
+      gender: "Male",
+      race: "Human",
+      height: ["6'1", "185 cm"],
+      weight: ["450 lb", "203 kg"],
+      eyeColor: "Brown",
+      hairColor: "Brown",
+    },
+    biography: {
+      fullName: "William Baker",
+      alterEgos: "No alter egos found.",
+      aliases: ["Flint Marko", "Sylvester Mann", "Quarryman"],
+      placeOfBirth: "Queens, New York",
+      firstAppearance: "Amazing Spider-Man #4 (September, 1963)",
+      publisher: "Marvel Comics",
+      alignment: "neutral",
+    },
+    work: {
+      occupation: "Professional Criminal",
+      base: "-",
+    },
+    connections: {
+      groupAffiliation:
+        "(Formerly) Sinister Twelve, the Sinister Six, the Frightful Four, the Outlaws and the Wild Pack, reserve member of the Avengers",
+      relatives: "Mrs. Baker (mother), unidentified cousin",
+    },
+    images: {
+      xs:
+        "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/xs/572-sandman.jpg",
+      sm:
+        "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/sm/572-sandman.jpg",
+      md:
+        "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/md/572-sandman.jpg",
+      lg:
+        "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/lg/572-sandman.jpg",
+    },
+  },
+  {
+    id: 589,
+    name: "She-Hulk",
+    slug: "589-she-hulk",
+    powerstats: {
+      intelligence: 81,
+      strength: 100,
+      speed: 42,
+      durability: 100,
+      power: 40,
+      combat: 70,
+    },
+    appearance: {
+      gender: "Female",
+      race: "Human",
+      height: ["6'7", "201 cm"],
+      weight: ["700 lb", "315 kg"],
+      eyeColor: "Green",
+      hairColor: "Green",
+    },
+    biography: {
+      fullName: "Jennifer Walters",
+      alterEgos: "No alter egos found.",
+      aliases: ["Jade Giantess"],
+      placeOfBirth: "Los Angeles, California",
+      firstAppearance: "She-Hulk (Vol. 1) #1",
+      publisher: "Marvel Comics",
+      alignment: "good",
+    },
+    work: {
+      occupation: "Lawyer, Magistra of the Living Tribunal",
+      base:
+        "Law offices of Goodman, Lieber, Kurtzberg, & Holliway; formerly Avengers Mansion; Baxter Building, Four Freedoms Plaza",
+    },
+    connections: {
+      groupAffiliation:
+        "Goodman, Lieber, Kurtzberg, & Holliway; formerly Avengers, Fantastic Four, Heroes for Hire",
+      relatives: "Bruce Banner (Hulk, cousin)",
+    },
+    images: {
+      xs:
+        "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/xs/589-she-hulk.jpg",
+      sm:
+        "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/sm/589-she-hulk.jpg",
+      md:
+        "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/md/589-she-hulk.jpg",
+      lg:
+        "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/lg/589-she-hulk.jpg",
+    },
+  },
+  {
+    id: 619,
+    name: "Spider-Gwen",
+    slug: "619-spider-gwen",
+    powerstats: {
+      intelligence: 75,
+      strength: 55,
+      speed: 63,
+      durability: 70,
+      power: 66,
+      combat: 70,
+    },
+    appearance: {
+      gender: "Female",
+      race: "Human",
+      height: ["5'5", "165 cm"],
+      weight: ["125 lb", "56 kg"],
+      eyeColor: "Blue",
+      hairColor: "Blond",
+    },
+    biography: {
+      fullName: "Gwen Stacy",
+      alterEgos: "No alter egos found.",
+      aliases: ["Spider-Woman"],
+      placeOfBirth: "-",
+      firstAppearance: "Edge of Spider-Verse #2",
+      publisher: "Marvel Comics",
+      alignment: "good",
+    },
+    work: {
+      occupation: "-",
+      base: "-",
+    },
+    connections: {
+      groupAffiliation:
+        "Warriors of the Great Web; formerly Mary Janes, Spider-Army",
+      relatives: "George Stacy (father), Helen Stacy (mother, deceased)",
+    },
+    images: {
+      xs:
+        "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/xs/619-spider-gwen.jpg",
+      sm:
+        "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/sm/619-spider-gwen.jpg",
+      md:
+        "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/md/619-spider-gwen.jpg",
+      lg:
+        "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/lg/619-spider-gwen.jpg",
+    },
+  },
+  {
+    id: 620,
+    name: "Spider-Man",
+    slug: "620-spider-man",
+    powerstats: {
+      intelligence: 90,
+      strength: 55,
+      speed: 67,
+      durability: 75,
+      power: 74,
+      combat: 85,
+    },
+    appearance: {
+      gender: "Male",
+      race: "Human",
+      height: ["5'10", "178 cm"],
+      weight: ["165 lb", "74 kg"],
+      eyeColor: "Hazel",
+      hairColor: "Brown",
+    },
+    biography: {
+      fullName: "Peter Parker",
+      alterEgos: "No alter egos found.",
+      aliases: [
+        "Spiderman",
+        "Bag-Man",
+        "Black Marvel",
+        "Captain Universe",
+        "Dusk",
+        "Green Hood",
+        "Hornet",
+        "Mad Dog 336",
+        "Peter Palmer",
+        "Prodigy",
+        "Ricochet",
+        "Scarlet Spider",
+        "Spider-Boy",
+        "Spider-Hulk",
+        "Spider-Morphosis",
+      ],
+      placeOfBirth: "New York, New York",
+      firstAppearance: "Amazing Fantasy #15",
+      publisher: "Marvel Comics",
+      alignment: "good",
+    },
+    work: {
+      occupation: "Freelance photographer, teacher",
+      base: "New York, New York",
+    },
+    connections: {
+      groupAffiliation:
+        "Member of the Avengers, formerly member of Outlaws, alternate Fantastic Four",
+      relatives:
+        "Richard Parker (father, deceased), Mary Parker(mother, deceased), Benjamin Parker (uncle, deceased), May Parker (aunt), Mary Jane Watson-Parker (wife), May Parker (daughter, allegedly deceased)",
+    },
+    images: {
+      xs:
+        "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/xs/620-spider-man.jpg",
+      sm:
+        "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/sm/620-spider-man.jpg",
+      md:
+        "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/md/620-spider-man.jpg",
+      lg:
+        "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/lg/620-spider-man.jpg",
+    },
+  },
+  {
+    id: 623,
+    name: "Spider-Woman",
+    slug: "623-spider-woman",
+    powerstats: {
+      intelligence: 56,
+      strength: 42,
+      speed: 42,
+      durability: 60,
+      power: 68,
+      combat: 70,
+    },
+    appearance: {
+      gender: "Female",
+      race: "Human",
+      height: ["5'10", "178 cm"],
+      weight: ["130 lb", "59 kg"],
+      eyeColor: "Green",
+      hairColor: "Black",
+    },
+    biography: {
+      fullName: "Jessica Drew",
+      alterEgos: "No alter egos found.",
+      aliases: [
+        "Arachne",
+        "Ariadne Hyde",
+        "Dark Angel",
+        "Dark Angel of San Francisco",
+      ],
+      placeOfBirth: "London, England",
+      firstAppearance: "Marvel Spotlight #32 (February, 1977)",
+      publisher: "Marvel Comics",
+      alignment: "good",
+    },
+    work: {
+      occupation:
+        "Form agent of HYDRA, former bounty hunter, private investigator, adventurer",
+      base: "-",
+    },
+    connections: {
+      groupAffiliation: "Former agent of HYDRA",
+      relatives: "Jonathan (father, deceased), Merriem (mother, deceased)",
+    },
+    images: {
+      xs:
+        "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/xs/623-spider-woman.jpg",
+      sm:
+        "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/sm/623-spider-woman.jpg",
+      md:
+        "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/md/623-spider-woman.jpg",
+      lg:
+        "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/lg/623-spider-woman.jpg",
+    },
+  },
+  {
+    id: 644,
+    name: "Superman",
+    slug: "644-superman",
+    powerstats: {
+      intelligence: 94,
+      strength: 100,
+      speed: 100,
+      durability: 100,
+      power: 100,
+      combat: 85,
+    },
+    appearance: {
+      gender: "Male",
+      race: "Kryptonian",
+      height: ["6'3", "191 cm"],
+      weight: ["225 lb", "101 kg"],
+      eyeColor: "Blue",
+      hairColor: "Black",
+    },
+    biography: {
+      fullName: "Clark Kent",
+      alterEgos: "Superman Prime One-Million",
+      aliases: [
+        "Clark Joseph Kent",
+        "The Man of Steel",
+        "the Man of Tomorrow",
+        "the Last Son of Krypton",
+        "Big Blue",
+        "the Metropolis Marvel",
+        "the Action Ace",
+      ],
+      placeOfBirth: "Krypton",
+      firstAppearance: "ACTION COMICS #1",
+      publisher: "Superman Prime One-Million",
+      alignment: "good",
+    },
+    work: {
+      occupation: "Reporter for the Daily Planet and novelist",
+      base: "Metropolis",
+    },
+    connections: {
+      groupAffiliation:
+        "Justice League of America, The Legion of Super-Heroes (pre-Crisis as Superboy); Justice Society of America (pre-Crisis Earth-2 version); All-Star Squadron (pre-Crisis Earth-2 version)",
+      relatives:
+        "Lois Lane (wife), Jor-El (father, deceased), Lara (mother, deceased), Jonathan Kent (adoptive father), Martha Kent (adoptive mother), Seyg-El (paternal grandfather, deceased), Zor-El (uncle, deceased), Alura (aunt, deceased), Supergirl (Kara Zor-El, cousin), Superboy (Kon-El/Conner Kent, partial clone)",
+    },
+    images: {
+      xs:
+        "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/xs/644-superman.jpg",
+      sm:
+        "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/sm/644-superman.jpg",
+      md:
+        "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/md/644-superman.jpg",
+      lg:
+        "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/lg/644-superman.jpg",
+    },
+  },
+  {
+    id: 659,
+    name: "Thor",
+    slug: "659-thor",
+    powerstats: {
+      intelligence: 69,
+      strength: 100,
+      speed: 83,
+      durability: 100,
+      power: 100,
+      combat: 100,
+    },
+    appearance: {
+      gender: "Male",
+      race: "Asgardian",
+      height: ["6'6", "198 cm"],
+      weight: ["640 lb", "288 kg"],
+      eyeColor: "Blue",
+      hairColor: "Blond",
+    },
+    biography: {
+      fullName: "Thor Odinson",
+      alterEgos: "Rune King Thor",
+      aliases: [
+        "Donald Blake",
+        "Sigurd Jarlson",
+        "Jake Olsen",
+        "Donar the Mighty",
+      ],
+      placeOfBirth: "Asgard",
+      firstAppearance: "Journey into Mystery #83 (August, 1962)",
+      publisher: "Rune King Thor",
+      alignment: "good",
+    },
+    work: {
+      occupation: "King of Asgard; formerly EMS Technician; Physician",
+      base: "New York, New York",
+    },
+    connections: {
+      groupAffiliation: "Avengers",
+      relatives:
+        "Odin (father), Gaea (mother), Frigga (step-mother), Loki (step-brother), Vidar (half-brother), Buri (paternal great-grandfather), Bolthorn (maternal great grandfather), Bor (grandfather), Bestla (grandmother), Vili (uncle), Ve (uncle), Sigyn (former sister-in-law), Hela (alleged niece), Jormungand (alleged nephew), Fernis Wolf (alleged nephew)",
+    },
+    images: {
+      xs:
+        "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/xs/659-thor.jpg",
+      sm:
+        "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/sm/659-thor.jpg",
+      md:
+        "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/md/659-thor.jpg",
+      lg:
+        "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/lg/659-thor.jpg",
+    },
+  },
+  {
+    id: 370,
+    name: "Joker",
+    slug: "370-joker",
+    powerstats: {
+      intelligence: 100,
+      strength: 10,
+      speed: 12,
+      durability: 60,
+      power: 43,
+      combat: 70,
+    },
+    appearance: {
+      gender: "Male",
+      race: "Human",
+      height: ["6'5", "196 cm"],
+      weight: ["192 lb", "86 kg"],
+      eyeColor: "Green",
+      hairColor: "Green",
+    },
+    biography: {
+      fullName: "Jack Napier",
+      alterEgos: "No alter egos found.",
+      aliases: [
+        "Red Hood I",
+        "Clown Prince of Crime",
+        "Harlequin of Hate",
+        "Jack Napier",
+        "Joe Kerr",
+        "Mr. J",
+      ],
+      placeOfBirth: "-",
+      firstAppearance: "Batman #1 (Spring 1940)",
+      publisher: "DC Comics",
+      alignment: "bad",
+    },
+    work: {
+      occupation: "-",
+      base: "Arkham Asylum, Gotham City; Ha-Hacienda",
+    },
+    connections: {
+      groupAffiliation:
+        "Black Glove, Injustice Gang, Injustice League, Joker League of Anarchy",
+      relatives:
+        "Jeannie (wife, deceased); Unborn son (deceased); Melvin Reipan (cousin, deceased)",
+    },
+    images: {
+      xs:
+        "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/xs/370-joker.jpg",
+      sm:
+        "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/sm/370-joker.jpg",
+      md:
+        "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/md/370-joker.jpg",
+      lg:
+        "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/lg/370-joker.jpg",
+    },
+  },
+  {
+    id: 384,
+    name: "Kid Flash",
+    slug: "384-kid-flash",
+    powerstats: {
+      intelligence: 25,
+      strength: 4,
+      speed: 92,
+      durability: 42,
+      power: 20,
+      combat: 30,
+    },
+    appearance: {
+      gender: "Male",
+      race: "Human",
+      height: ["-", "0 cm"],
+      weight: ["- lb", "0 kg"],
+      eyeColor: "Green",
+      hairColor: "Red",
+    },
+    biography: {
+      fullName: "Wallace Rudolph West",
+      alterEgos: "No alter egos found.",
+      aliases: ["Kid Lantern"],
+      placeOfBirth: "Blue Valley, Nebraska",
+      firstAppearance: "Flash #110 (January, 1960)",
+      publisher: "DC Comics",
+      alignment: "good",
+    },
+    work: {
+      occupation: "-",
+      base: "-",
+    },
+    connections: {
+      groupAffiliation: "Flash Family, Teen Titans, New Teen Titans",
+      relatives:
+        "Rudolph and Mary West (parents), Ira West (grandfather), Iris Allen (aunt), Barry Allen (uncle), Linda Park West (wife), Jai and Iris West (twin children)",
+    },
+    images: {
+      xs:
+        "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/xs/384-kid-flash.jpg",
+      sm:
+        "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/sm/384-kid-flash.jpg",
+      md:
+        "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/md/384-kid-flash.jpg",
+      lg:
+        "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/lg/384-kid-flash.jpg",
+    },
+  },
+  {
+    id: 395,
+    name: "Kraven the Hunter",
+    slug: "395-kraven-the-hunter",
+    powerstats: {
+      intelligence: 63,
+      strength: 32,
+      speed: 35,
+      durability: 42,
+      power: 25,
+      combat: 85,
+    },
+    appearance: {
+      gender: "Male",
+      race: "Human",
+      height: ["6'0", "183 cm"],
+      weight: ["235 lb", "106 kg"],
+      eyeColor: "Brown",
+      hairColor: "Black",
+    },
+    biography: {
+      fullName: "Sergei Kravinoff",
+      alterEgos: "No alter egos found.",
+      aliases: [
+        "Kraven the Hunter",
+        "World's Greatest Hunter",
+        "the Grim Hunter",
+      ],
+      placeOfBirth: "Volgograd (formerly Stalingrad), Russia",
+      firstAppearance: "The Amazing Spider-Man #15 (1964)",
+      publisher: "Marvel Comics",
+      alignment: "bad",
+    },
+    work: {
+      occupation: "Professional game hunter, mercenary",
+      base: "-",
+    },
+    connections: {
+      groupAffiliation: "(formerly) The Sinister Six",
+      relatives:
+        'Nikolai & Anna Makarova Kravinoff (parents, deceased), Dmitri Smerdyakov (Chameleon, half-brother), Vladimir Kravinoff (Grim Hunter, son, deceased), Aloysha "Al" Kravinoff (Kraven II, son), Ned Tannengarden (son, deceased)',
+    },
+    images: {
+      xs:
+        "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/xs/395-kraven-the-hunter.jpg",
+      sm:
+        "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/sm/395-kraven-the-hunter.jpg",
+      md:
+        "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/md/395-kraven-the-hunter.jpg",
+      lg:
+        "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/lg/395-kraven-the-hunter.jpg",
+    },
+  },
+  {
+    id: 322,
+    name: "Hellboy",
+    slug: "322-hellboy",
+    powerstats: {
+      intelligence: 63,
+      strength: 53,
+      speed: 21,
+      durability: 95,
+      power: 73,
+      combat: 75,
+    },
+    appearance: {
+      gender: "Male",
+      race: "Demon",
+      height: ["8'6", "259 cm"],
+      weight: ["350 lb", "158 kg"],
+      eyeColor: "Gold",
+      hairColor: "Black",
+    },
+    biography: {
+      fullName: "Anung Un Rama",
+      alterEgos: "No alter egos found.",
+      aliases: ["World Destroyer", "The Great Beast"],
+      placeOfBirth: "-",
+      firstAppearance: "Dime Press #4 (May 1, 1993)",
+      publisher: "Dark Horse Comics",
+      alignment: "good",
+    },
+    work: {
+      occupation: "-",
+      base: "-",
+    },
+    connections: {
+      groupAffiliation:
+        "Abe Sapien, Liz Sherman, Roger the Homunculus, Savage Dragon, the Goon, Batman, Starman, Ghost, Painkiller Jane, Lobster Johnson, Torch of Liberty",
+      relatives:
+        "Catherine Tanner-Tremaine (mother, deceased), two unnamed maternal half-siblings (deceased), Professor Trevor Bruttenholm (foster father)",
+    },
+    images: {
+      xs:
+        "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/xs/322-hellboy.jpg",
+      sm:
+        "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/sm/322-hellboy.jpg",
+      md:
+        "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/md/322-hellboy.jpg",
+      lg:
+        "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/lg/322-hellboy.jpg",
+    },
+  },
+];
